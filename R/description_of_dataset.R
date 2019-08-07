c2c45f09e621cc8d0c8c45ac3d842ead4d77b128
lschlicht <lschlicht@orn.mpg.de>
2019-08-07 15:26:31 +0200

    repair package

---
+++ R/description_of_dataset.R
@@ -0,0 +1,12 @@
+#' Full dataset
+#'
+#' @docType data
+#'
+#' @usage data(BTemergenceData)
+#'
+#' @format An object of class \code{"data.table"}.
+#'
+#' @keywords datasets
+#'
+#'
+"x2"
--- R/functions.r
+++ R/functions.r
@@ -1,4 +1,19 @@
 
+#' Wrapper function for gamm4()
+#'
+#' @param response Response variable
+#' @param by_gamm "By" for the smoother
+#' @param k_gamm "k" that si supplied to gamm4
+#' @param explanatory explanatory variable
+#' @param data dataset
+#' @param rm.intercept Should the intercet be removed when calculating the model (0+ added to the model formula on the right side)
+#' @param smooth_over Which variable to smooth over (here date or days to first egg)
+#' @return The output of a gamm4.
+#' @import data.table
+#' @import stats
+#' @import gamm4
+#' @export
+#'
 gamm4.2 = function(response, by_gamm, k_gamm = 40, explanatory = c(NULL), data, rm.intercept = FALSE, smooth_over = 'rel_day') {
   if(rm.intercept == TRUE & is.null(explanatory)) print("If rm.intercept == TRUE then explanatory variables must be given.")
 
@@ -11,13 +26,27 @@ gamm4.2 = function(response, by_gamm, k_gamm = 40, explanatory = c(NULL), data, 
 
 
 
-create_gamm_plot = function(var, data = x2, include_age = FALSE, modelset = list(), COL = c("red", "blue"), plot_sex = NULL, plot_which = NULL, ...) {
+#' Make Figures 1-3 (smoothed figures): Subfunction
+#'
+#' @param var variable to plot, e.g. time_to_sunset_min
+#' @param data dataset
+#' @param include_age Should age be included?
+#' @param modelset Model for smooth line
+#' @param COL Colors for males and females
+#' @param plot_sex obsolete?
+#' @param plot_which obsolete?
+#' @param ... passed on to plot.
+#' @import stats
+#' @return A panel of figures 1-3
+#' @export
+#'
+create_gamm_plot = function(var, data, include_age = FALSE, modelset = list(), COL = c("red", "blue"), plot_sex = NULL, plot_which = NULL, ...) {
 
-  SHIFT1 = summary(modelset[[1]]$gam)$p.table[1,1]#  + mean(plot(modelset[[1]]$gam, select = 0)[[1]]$fit)
-  SHIFT2 = summary(modelset[[2]]$gam)$p.table[1,1]# + mean(plot(modelset[[2]]$gam, select = 0)[[1]]$fit)
+  SHIFT1 = summary(modelset[[1]]$gam)$p.table[1,1]
+  SHIFT2 = summary(modelset[[2]]$gam)$p.table[1,1]
 
   #plot
-  plot.gam(modelset[[1]]$gam, shift = SHIFT1, col = COL[1], select = 1, rug = FALSE, xaxt = 'n', ...)
+  plot(modelset[[1]]$gam, shift = SHIFT1, col = COL[1], select = 1, rug = FALSE, xaxt = 'n', ...)
   axis(1, labels = FALSE, tick = TRUE)
   par(new = TRUE)
   plot(modelset[[2]]$gam, shift = SHIFT2, col = COL[2], select = 1, rug = FALSE, xaxt = 'n', ...)
@@ -25,6 +54,15 @@ create_gamm_plot = function(var, data = x2, include_age = FALSE, modelset = list
 
 
 
+#' Plot environmental variables (Figure S1)
+#'
+#' @param df Data
+#' @param VARIABLE variable to plot.
+#' @param ... further parameters
+#' @return A figure
+#' @import graphics
+#' @export
+#'
 plot_env = function(df, VARIABLE, ...) {
   plot(c(-2, 3.0), c(0.5, 2.5), type = "n", yaxt = "n", ylab = "", ...)
   arrows(df[variable == VARIABLE, low], df[variable == VARIABLE, YY], df[variable == VARIABLE, up], df[variable == VARIABLE, YY], code = 3, angle = 90, length = 0.15, col = df[variable == VARIABLE, COL])
@@ -44,6 +82,14 @@ plot_env = function(df, VARIABLE, ...) {
 
 }
 
+#' Helper function for Fig. 1-3
+#'
+#' @param d1 First list
+#' @param d2 Second list
+#' @param ... further arguments
+#' @import graphics
+#' @export
+#'
 boxplot_emergence = function(d1, d2, ...) {
   plot(c(-100, 90), c(-60, 70), type = 'n', ...)
   points(d1$YDAY, d1$med, pch = 16, ...)
@@ -53,6 +99,19 @@ boxplot_emergence = function(d1, d2, ...) {
 }
 
 
+#' Pipeline function for Figure 1-3
+#' @details Uses the functions boxplot_emergence and create_gamm_plot.
+#' @param modellist_yday Modellist for "Day of year"
+#' @param modellist_rel_day Modellist for "Days to first egg"
+#' @param d_list Boxplot variables
+#' @param out_list Boxplot variables
+#' @param YLAB ylab
+#' @param VAR which variable is plotted?
+#' @param ylim_boxplot ylim of boxplot
+#' @param ylim_gammplot yoim of gamm-plot
+#' @return Figure like Figures 1-3
+#' @import graphics
+#' @export
 plot_descriptive_figure = function(modellist_yday, modellist_rel_day, d_list, out_list, YLAB, VAR,ylim_boxplot = c(-60, 70), ylim_gammplot = c(-40, 20)) {
     #figure settings
     cex.pt = 2
@@ -107,7 +166,20 @@ plot_descriptive_figure = function(modellist_yday, modellist_rel_day, d_list, ou
 
 
 
-plot_individuals = function(data = x, i = 1, yead_ID = year_ID, var = "time_to_sunrise_min", MALE_COLOUR = "#5000FFFF", FEMALE_COLOUR = "#FF758AFF", plot.lines = FALSE, ...) {
+#' Plot individuals
+#'
+#' @param data Dataset
+#' @param i Number of the individual within data that should be plotted
+#' @param year_ID year_ and ID pasted together
+#' @param var variable to plot (e.g. time_to_sunset_min)
+#' @param MALE_COLOUR colour of males
+#' @param FEMALE_COLOUR colour of females
+#' @param plot.lines logical. Should points within a week be connected by lines?
+#' @param ... Further arguments passed on to plot
+#' @import graphics
+#' @return A figure such as S3-S4
+#' @export
+plot_individuals = function(data, i = 1, year_ID, var = "time_to_sunrise_min", MALE_COLOUR = "#5000FFFF", FEMALE_COLOUR = "#FF758AFF", plot.lines = FALSE, ...) {
 
   N = subset(data, ID == year_ID[i, ID] & breeding_season == year_ID[i, breeding_season])
   N[, var := N[,which(names(N) == var), with = FALSE]]
@@ -129,6 +201,13 @@ plot_individuals = function(data = x, i = 1, yead_ID = year_ID, var = "time_to_s
 
 }
 
+#' Helper function to check the fit of the model assumptions
+#'
+#' @param mod gamm model to plot
+#' @return A figure
+#' @import graphics
+#' @import stats
+#' @export
 plot_gamm_models = function(mod) {
   par(mfrow = c(2,2))
   qqnorm(resid(mod$mer))
@@ -137,7 +216,18 @@ plot_gamm_models = function(mod) {
   plot(0:1, 0:1, type = 'n')
 }
 
-plot_seasonal_progress = function(tmp1, tmp2, YLIM = c(-10, 5), XLAB = "Onset of activity", YLAB = paste0("average Temperature\r\n", ifelse(i == 1, "males", "females"))) {
+#' Plots how the effect sizes vary with season (Figs. S7-10)
+#'
+#' @param tmp1 The data for day of year
+#' @param tmp2 The data for relative day
+#' @param YLIM ylim
+#' @param XLAB xlab
+#' @param YLAB ylab
+#' @return A figure such as Figs. S7-10
+#' @import graphics
+#' @export
+plot_seasonal_progress = function(tmp1, tmp2, YLIM = c(-10, 5), XLAB = "Onset of activity", YLAB = NULL) {
+  if(is.null(YLAB)) YLAB = paste0("average Temperature\r\n", ifelse(i == 1, "males", "females"))
   par(mfrow = c(2,1))
   par(cex = 2, lwd = 2, cex.axis = 0.8)
   for(i in 1 : 2) {
